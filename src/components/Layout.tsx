70ea4a52c44904b1a282313e4c2b1b69ddda4fb9
Gustavo <elmergustavo79@gmail.com>
2023-01-21 17:47:58 -0600

    fixed layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import Footer from "./Navigation/Footer";
 import Header from "./Navigation/Header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full h-full transition-all duration-300 max-w-screen-lg mx-auto overflow-hidden">
+    <div className="w-full h-full transition-all duration-300 px-4 md:max-w-screen-lg md:mx-auto overflow-hidden">
       <Header /> {children} <Footer />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
       <div id="repository" className="mt-28" data-aos="fade-left">
         <div id="title" className="relative">
-          <h2 className="text-5xl ml-20 absolute pointer-events-none absolute left-24 -top-4 z-0 -translate-x-1/2 transform font-bold uppercase text-heading opacity-15 text-white opacity-20">
+          <h2 className="text-5xl ml-20  pointer-events-none absolute left-24 -top-4 z-0 -translate-x-1/2 transform font-bold uppercase text-heading opacity-15 text-white opacity-20">
             Repositories
           </h2>
           <h2 className="text-3xl font-semibold text-white ml-[18px] -pt-4 ">
@@ -43,7 +43,7 @@ export default function Home() {
 
       <div id="skills" className="mt-20" data-aos="fade-right">
         <div id="header" className="relative ml-[8px]">
-          <h2 className="text-6xl absolute pointer-events-none absolute left-24 -top-4 z-0 -translate-x-1/2 transform font-bold uppercase text-heading opacity-15 text-white opacity-20">
+          <h2 className="text-6xl  pointer-events-none absolute left-24 -top-4 z-0 -translate-x-1/2 transform font-bold uppercase text-heading opacity-15 text-white opacity-20">
             Skills
           </h2>
           <h2 className="text-3xl font-semibold text-white pt-2 ml-[18px]">
