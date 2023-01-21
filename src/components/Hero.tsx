63588e8228a2f998d93dd62ff368c1236de2b860
Gustavo <elmergustavo79@gmail.com>
2023-01-20 22:28:35 -0600

    fixed errors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -62,7 +62,7 @@ export default function Hero() {
       <h1 className="text-5xl font-semibold text-white">
         <span className="text-primary font-bold">I’m Gustavo ✌️</span> 
       </h1>
-      <p className="mt-4 text-md text-gray-200">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p className="mt-4 text-md text-gray-200">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
     </div>
   );
 }
